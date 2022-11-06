e33400ed6adb34f1a46d3cc9f38cadafc184581e
Thomas Dingemanse <thomas.dingemanse@gmail.com>
2022-11-06 16:26:44 +0100

    Fix algorithm and parameters

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import numpy as np
+import random
 
 # T     = Time periods
 # x_max = max inventory
@@ -10,12 +11,19 @@ import numpy as np
 # Q     = vector with value by action
 
 T = 10
-x_max = 10
 
-# Parameters
-d = 2
-h = 1
-K = 5
+# We can buy any number of items, but since demand is at most 1 for each time
+# step
+x_max = T
+
+# Probability of d=1 by time step
+P = [_ * 0.001 for _ in range(T)]
+D = [int(random.uniform(0, 1)< P[_]) for _ in range(T)]
+
+h = 0.01
+selling_price = 20
+purchase_cost = lambda _ : 10 if _ <= 500 else 15
+K = 10
 
 # Value function
 V = np.zeros((T, x_max))
@@ -24,7 +32,7 @@ V = np.zeros((T, x_max))
 alpha = np.zeros((T, x_max))
 
 # vector with value of actions
-Q = np.zeros((100))
+Q = np.zeros((x_max))
 
 # Set value for last time step (value = items sold * price)
 V[0:,T-1] = [h * _  for _ in range(x_max)]
@@ -32,6 +40,7 @@ V[0:,T-1] = [h * _  for _ in range(x_max)]
 # For each time step (going backwards from t=T)
 for t in reversed(range(1, T-1)):
     # For each state (number of items in inventory)
+    d = D[t]
     for x in range(0, x_max):
         # For each possible action (number of items ordered)
         a_min = max(d-x, 0)
@@ -39,7 +48,9 @@ for t in reversed(range(1, T-1)):
         for a in range(a_min, a_max):
             # Calculate value for the given action
             holding_costs = x * h
-            order_costs = K * int(a > 0)
+
+            # no purchases possible after time 900
+            order_costs = K * int(a > 0) if t <= 900 else 0
             # t+1 = next time interval (looking forward)
             Q[a] = holding_costs + order_costs + V[x - d + a, t + 1]
         
