4566121b2c429491013dc9a5a926a69cebcffd8b
Hug0Z <90772478+Hug0Z@users.noreply.github.com>
2022-11-09 17:11:26 +0100

    Created simulations
    Simulations now work for the assignments

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
-import numpy as np
 import random
 
+import numpy as np
+
+
 class StochasticInventoryProblem:
-    def __init__(self, T = 1000, x_max = 100):
+    def __init__(self, T=1000, x_max=100):
         # Number of time steps (days)
         self.T = T
 
@@ -23,7 +25,7 @@ class StochasticInventoryProblem:
 
         # Dynamic buying price and static selling price
         self.selling_price = 20
-        self.purchase_cost = lambda _ : 10 if _ <= 500 else 15
+        self.purchase_cost = lambda _: 10 if _ <= 500 else 15
 
         # Value for each time and state
         self.V = np.zeros((self.x_max + 1, self.T))
@@ -35,18 +37,18 @@ class StochasticInventoryProblem:
         self.Q = np.zeros((self.x_max + 1))
 
         # The value at the last time step is the value of the inventory
-        self.V[0:self.x_max, self.T - 1] = [self.h * _  for _ in range(self.x_max)]
+        self.V[0 : self.x_max, self.T - 1] = [self.h * _ for _ in range(self.x_max)]
 
     # Reset values to run again with different parameters
     def reset(self):
-        self.__init__(T = self.T)
+        self.__init__(T=self.T)
 
     # Apply dynamic programming to find the optimal policy and its value
     def find_optimal_policy(self):
         # For each time step, going backwards from t = T - 1 to t = 0
         for t in reversed(range(0, self.T - 1)):
-            print(f"[{t}/{self.T - 2}]")
-            
+            # print(f"[{t}/{self.T - 2}]")
+
             # For each state (number of items in inventory)
             for x in range(0, self.x_max + 1):
                 # Determine the action space A
@@ -59,7 +61,11 @@ class StochasticInventoryProblem:
 
                 # For each legal action (number of items ordered)
                 for a in range(a_min, a_max):
-                    # print("state: {state} action: {action} next t: {t}".format(state=x, action=a, t=t + 1))
+                    # print(
+                    #     "state: {state} action: {action} next t: {t}".format(
+                    #         state=x, action=a, t=t + 1
+                    #     )
+                    # )
 
                     # Calculate value for the given action
                     holding_costs = x * self.h
@@ -76,31 +82,41 @@ class StochasticInventoryProblem:
                     # acquire cost
                     # No purchases possible after time 900
                     acquire_cost = self.purchase_cost(t) * a
-                    
+
                     # Calculate value for this time step
-                    self.Q[a] = holding_costs + order_costs + next_value + acquire_cost - earnings
-                
+                    self.Q[a] = (
+                        holding_costs
+                        + order_costs
+                        + next_value
+                        + acquire_cost
+                        - earnings
+                    )
+
                 # Initialize current best value and optimal policy
-                self.V[x,t] = self.Q[a_min]
-                self.alpha[x,t] = a_min
-                
+                self.V[x, t] = self.Q[a_min]
+                self.alpha[x, t] = a_min
+
                 # Choose best action and optimal policy for this state and time step
                 for a in range(a_min + 1, a_max):
-                    if self.Q[a] < self.V[x,t]:
-                        self.alpha[x,t] = a
-                        self.V[x,t] = self.Q[a]
+                    if self.Q[a] < self.V[x, t]:
+                        self.alpha[x, t] = a
+                        self.V[x, t] = self.Q[a]
 
     # Perform n simulations of the demand over time
-    def simulate(self, n = 10):
-        raise NotImplementedError()
+    def simulate(self, n=10):
+        for _ in range(n):
+            print(f"current run: {_+1}")
+            self.reset()
+            self.find_optimal_policy()
+            self.export(run_nr=_)
 
     # Export the value function and optimal policy
-    def export(self):
-        np.savetxt("value.csv", self.V, fmt='%d', delimiter=",")
-        np.savetxt("policy.csv", self.alpha, fmt='%d', delimiter=",")
+    def export(self, run_nr=0):
+        np.savetxt(f"value_{run_nr}.csv", self.V, fmt="%d", delimiter=",")
+        np.savetxt(f"policy_{run_nr}.csv", self.alpha, fmt="%d", delimiter=",")
+        np.savetxt(f"demand_{run_nr}.csv", self.D, fmt="%d", delimiter=",")
+
 
 if __name__ == "__main__":
     s = StochasticInventoryProblem()
-    s.find_optimal_policy()
-    s.export()
-    # s.simulate(n = 10)
+    s.simulate()
