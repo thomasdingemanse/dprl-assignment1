f74f5e14cf16259adc64d457b42380156984fd8e
HugoZ <hugozwaan2@gmail.com>
2022-11-10 19:03:25 +0100

    Missed sales data

--- transmute.py
+++ transmute.py
@@ -4,13 +4,15 @@ from numpy import genfromtxt
 df_data = {}
 
 for _ in range(10):
-    df = pd.read_csv(f"policy_{_}.csv")
+    df = pd.read_csv(f"Data/policy_{_}.csv")
     df.columns = [str(_) for _ in range(1000)]
-    demand = genfromtxt(f'demand_{_}.csv', delimiter=',')
+    demand = genfromtxt(f'Data/demand_{_}.csv', delimiter=',')
 
     inventory = []
     actions = []
+    missed_sales = []
     cur_inv = 0
+    missed = 0
     for i, dem in enumerate(demand):
         inventory.append(cur_inv)
 
@@ -23,8 +25,13 @@ for _ in range(10):
 
         cur_inv = max(cur_inv + action - dem, 0)
 
-        df_data[f"inventory {_}"] = inventory
-        df_data[f"actions {_}"] = actions
+        if cur_inv + action < dem:
+            missed += 1
+        missed_sales.append(missed)
+
+    df_data[f"inventory {_}"] = inventory
+    df_data[f"actions {_}"] = actions
+    df_data[f"missed sales {_}"] = missed_sales
 
 res = pd.DataFrame(df_data)
-res.to_csv(f"res.csv")
+res.to_csv(f"Data/missed.csv")
